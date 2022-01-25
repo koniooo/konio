ef40ccfce9ef724a37dce54890e90121df79e380
konio <kotogatari@gmail.com>
2022-01-26 01:52:49 +0900

    main

--- src/pizza-calc/components/AreaInput.tsx
+++ src/pizza-calc/components/AreaInput.tsx
@@ -1,8 +1,8 @@
 import { getRoundedNum } from "../lib/getRoundedNum";
 
-export const AreaInput = (props: any) => {
+export const AreaInput = (props) => {
   const areaRounded = getRoundedNum(props.area, 10);
-  const areaOnChange = (event: any) => {
+  const areaOnChange = (event) => {
     const input: number = Number(event.target.value);
     const diameterFromArea = Math.sqrt(input / Math.PI) * 2;
     const diameterFromAreaRounded = getRoundedNum(diameterFromArea, 10000);
