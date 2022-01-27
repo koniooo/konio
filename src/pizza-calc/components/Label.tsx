9b241e68fc01959c19f05e4fa1596aba42092d53
konio <kotogatari@gmail.com>
2022-01-28 02:21:02 +0900

    main

--- src/pizza-calc/components/Label.tsx
+++ src/pizza-calc/components/Label.tsx
@@ -4,16 +4,16 @@ export const Label = () => {
   return (
     <section className={`${styles.labelSec} ${styles.row}`}>
       <div>
-        <h2>直径📐</h2>
+        <h2>直径</h2>
       </div>
       <div>
-        <h2>価格💰</h2>
+        <h2>価格</h2>
       </div>
       <div className={styles.base}>
         <h2>
           基準の
           <br />
-          ピザ🍕
+          ピザ
         </h2>
       </div>
       <div>
@@ -22,7 +22,7 @@ export const Label = () => {
           <br />
           あたりの
           <br />
-          価格💰
+          価格
         </h2>
       </div>
       <div>
@@ -31,18 +31,9 @@ export const Label = () => {
           <br />
           ピザとの
           <br />
-          比較👍👎
+          比較
         </h2>
       </div>
-      {/* <div>
-        <h2>
-          1円
-          <br />
-          あたりの
-          <br />
-          面積🧀
-        </h2>
-      </div> */}
     </section>
   );
 };
