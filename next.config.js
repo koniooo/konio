f84e7e7e64998fa67cc3cbdde9905e395ee1a204
koniooo <kotogatari@gmail.com>
2023-05-17 21:39:08 +0900

    change sassOptions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: { fiber: false, includePaths: [path.join(__dirname, "styles")] },
 };
 
 module.exports = nextConfig;
