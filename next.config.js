e00ded2a5658fff70c7ee1e8de8205185bd9d52f
koniooo <kotogatari@gmail.com>
2023-05-17 21:48:47 +0900

    main

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: { fiber: false, includePaths: [path.join(__dirname, "styles")] },
